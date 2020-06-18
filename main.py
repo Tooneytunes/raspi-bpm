b5b19a14389f575581d27c5588f7338a66b6a8c0
Marco Breemhaar <marcobreemhaar@gmail.com>
2020-06-18 18:54:18 +0200

    Implement log for more accurate BPM readings

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import threading
 import sys
 import sounddevice as sd
+import numpy as np
+from scipy.stats import mode
+
 from bpm_detection import bpm_detector
 
 
@@ -10,6 +13,9 @@ class BPM:
         self.bpm = 0.0
         self.recording_thread = None
 
+        self.log_size = 5
+        self.log = np.zeros(self.log_size)
+
     def calculate_bpm(self, fs=44100, seconds=3):
         myrecording = sd.rec(int(seconds * fs), samplerate=fs, channels=1)
         sd.wait()  # Wait until recording is finished
@@ -18,13 +24,21 @@ class BPM:
             local_bpm = local_bpm[0]
         except IndexError:
             return
-        self.bpm = round(local_bpm, 1)
+
+        calculated_bpm = round(local_bpm, 1)
+        self.update_log(calculated_bpm)
+
+        mode_list, _ = mode(self.log)
+        self.bpm = mode_list[0]
 
     def try_recording(self):
         thread = self.recording_thread
         if thread is None or not thread.is_alive():
             self.recording_thread = threading.Thread(target=self.calculate_bpm)
             self.recording_thread.start()
+
+    def update_log(self, new_value):
+        self.log = np.append(self.log[1:], new_value)
 
     def run(self):
         while True:
